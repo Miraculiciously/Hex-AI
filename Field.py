5765f7423447921e01139b752e6970a60a790243
Miraculixilein <39239863+Miraculixilein@users.noreply.github.com>
2021-11-15 18:14:11 +0100

    Added background to field and testing

--- Field.py
+++ Field.py
@@ -1,55 +1,57 @@
 import tkinter as tk
-from math import cos, sin, radians
+from math import cos, sin, radians, sqrt
 
-from networkx.drawing.nx_pylab import draw
 
-from Board import Board
 
 class GUI:
-    def __init__(self):
-        self.app=tk.Tk()
-        self.canvas=tk.Canvas(self.app) 
-        self.scale =  20
-        self.frame_boundary = 20 # this needs to be adapted according to the scale but I'm working on it maybe tonight
-        # add canvas width and height
-        
-        # Add line for player colours
-        #self.canvas.create_line(10, 10, x_end, 10, fill="blue")
-
-    # def draw_hexagon(self, x, y) -> None:
-    #     x_start = x
-    #     y_start = y
-    #     for n in range(6):
-    #         x_end = x_start + self.scale * sin(radians(60*n))
-    #         y_end = y_start + self.scale * cos(radians(60*n))
-    #         self.canvas.create_line(x_start, y_start, x_end, y_end, fill="black")
-    #         x_start = x_end
-    #         y_start = y_end
+    def __init__(self, N=11):
+        # Initialise all that the baord needs
+        self.N = N
+        self.colors = ['white' for n in range(N**2)]
 
     def hex_coordinates(self, x, y):
         points = [x, y]
         for n in range(5):
-            points.append(points[2*n] + self.scale * sin(radians(60*n)) )
-            points.append(points[2*n + 1] + self.scale * cos(radians(60*n)) )
+            points.append(points[2*n] + self.side_length * sin(radians(60*n)) )
+            points.append(points[2*n + 1] + self.side_length * cos(radians(60*n)) )
         return points
 
     def draw_field(self) -> None:
+        # Initialise canvas
+        self.app=tk.Tk()
+        self.side_length =  30
+        self.frame_boundary = 1/sqrt(2)*self.side_length # this needs to be adapted according to the scale but I'm working on it maybe tonight
+        # add canvas width and height
+        self.height = self.frame_boundary/2 + (self.N-1)*self.side_length*(1+ 1/sqrt(2))
+        self.width = self.frame_boundary + (self.N + self.N/2) * self.side_length*sqrt(3)
+        self.canvas=tk.Canvas(self.app, width = self.width, height = self.height)
+        # draw canvas boundaries
+        self.canvas.create_polygon(0,0, sqrt(3)*(self.N+0.3)*self.side_length, 0, self.width/2, self.height/2, fill = "red")
+        self.canvas.create_polygon(self.width,self.height, self.width/2, self.height/2, self.width - sqrt(3)*(self.N+0.3)*self.side_length, self.height, fill = "red")
+        self.canvas.create_polygon(0, 0, 0, self.height, self.width - sqrt(3)*(self.N+0.3)*self.side_length, self.height, self.width/2, self.height/2, fill = "blue")
+        self.canvas.create_polygon(self.width, 0, sqrt(3)*(self.N+0.3)*self.side_length, 0, self.width/2, self.height/2,self.width, self.height,  fill = "blue")
         self.canvas.pack()
-        for x in range(board.N):
-            for y in range(board.N):
+        for x in range(self.N):
+            for y in range(self.N):
                 self.canvas.create_polygon(
                     *self.hex_coordinates(
-                        self.frame_boundary + 2.25*self.scale*(x+y/2) * sin(radians(60)),
-                        self.frame_boundary + y*1.6875*self.scale
+                        self.frame_boundary + sqrt(3)*self.side_length*(x+y/2),
+                        self.frame_boundary + 1.5*y*self.side_length
                         ),
-                        fill= "white",
-                        outline = "black"
+                        fill= self.colors[y*self.N + x],
+                        outline = "black",
+                        width = 1.25
                     )
         
-        self.app.mainloop()    
+        self.app.mainloop()
+    
+    def make_move(self, index, color: str):
+        assert(color == "red" or color == "blue")
+        self.colors[index] = color
 
 
 if __name__ == '__main__':
-    board = Board()
     gui = GUI()
     gui.draw_field()
+    #gui.make_move(3, "red")
+    #gui.draw_field()
